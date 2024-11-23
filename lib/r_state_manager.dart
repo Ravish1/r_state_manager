d5fd4f938c148629eec9471023761974e1f6d7bd
Ravish1 <ravishsharma310@gmail.com>
2024-11-23 14:08:49 +0530

    Removed Example Files from src

--- lib/r_state_manager.dart
+++ lib/r_state_manager.dart
@@ -7,8 +7,5 @@ export 'src/r_state_manager.dart';
 export 'src/r_state.dart';
 export 'src/r_consumer.dart';
 export 'src/r_state_event.dart';
-export 'src/user_state.dart';
-export 'src/user_event.dart';
-export 'src/user_manager.dart';
 
 // TODO: Export any libraries intended for clients of this package.
